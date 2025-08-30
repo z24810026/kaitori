257741eed2a75731c877aa016b99faf9611ceef6
z24810026 <z24810026@gmail.com>
2025-08-30 12:37:50 +0800

    123

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <div style={{ padding: 24 }}>
-      <h1>歡迎來到網站</h1>
-      <p>這裡是給客人看的公開首頁內容。</p>
-      <a href="/admin/login">管理者登入</a>
+      <h1>ユーザー画面（開発中）</h1>
+      <a href="/admin/login">管理者画面に移動</a>
     </div>
   );
 }
